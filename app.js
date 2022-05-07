15f940ca14cb5bd229e494508aa6cd9723b39d1f
ryanh6 <ryanhohoho6@gmail.com>
2022-05-07 18:04:55 -0400

    Basic javascript tests

---
+++ app.js
@@ -0,0 +1,24 @@
+//Comments?
+//Print into the html website
+
+document.write("Hello this line comes from the app.js javacript file<br>");
+
+//Create and print a random number
+let x = Math.floor((Math.random()*4));
+document.write("Random Number is: ", x, "<br>");
+
+//Making arrays
+a = ["A", "B", "C", "D"];
+document.write(" Array: ", a, " (Oh its there)<br>");
+
+//Combining this together to choose a random element of array a
+document.write("A RANDOM LETTER FROM ARRAY A: ", a[x], "<br>");
+
+//ADDING ELEMENTS TO ARRAY MADNESS
+a.push("E");
+a.push("F");
+
+x = Math.floor((Math.random()*a.length));
+document.write("NEW Random Number is: ", x, "<br>");
+document.write("NEW Array: ", a, " (Oh its there)<br>");
+document.write("A DIFFERENT RANDOM LETTER FROM ARRAY A: ", a[x], "<br>");
