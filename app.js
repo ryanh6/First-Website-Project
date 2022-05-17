63b0f340be658667f1362641a357ca7c5832894b
ryanh6 <ryanhohoho6@gmail.com>
2022-05-17 12:27:52 -0400

    Random 36 images successful

--- app.js
+++ app.js
@@ -33,6 +33,9 @@ window.onload = function()
 
 function page2LoadUp()
 {
+    /*
+    //PREVIOUS TEST USING LETTERS FIRST
+
     wordArray = [ ["A", "B", "C", "D", "E", "F"],
                    ["G", "H", "I", "J", "K", "L"],
                    ["M", "N", "O", "P", "Q", "R"],
@@ -40,15 +43,20 @@ function page2LoadUp()
                    ["Y", "Z", "a", "b", "c", "d"],
                    ["e", "f", "g", "h", "i", "j"] ]
 
-    imageArray = ["Page2Images/1RedDesserts1.jpg", "Page2Images/1RedDesserts2.jpg"]
-
-    /*
     var text = document.getElementById("demo");
     var randomNumber = Math.floor((Math.random()*wordArray.length));
     text.innerHTML = (wordArray[randomNumber]);
     */
 
+
+    imageArray = ["Page2Images/1RedDesserts1.jpg", "Page2Images/1RedDesserts2.jpg", "Page2Images/1RedDesserts3.jpg", "Page2Images/1RedDesserts4.jpg", "Page2Images/1RedDesserts5.jpg", "Page2Images/1RedDesserts6.jpg",
+                  "Page2Images/2OrangeTiger1.jpg", "Page2Images/2OrangeTiger2.jpg", "Page2Images/2OrangeTiger3.jpg", "Page2Images/2OrangeTiger4.jpg", "Page2Images/2OrangeTiger5.jpg", "Page2Images/2OrangeTiger6.jpg",
+                  "Page2Images/3YellowRubber1.jpg", "Page2Images/3YellowRubber2.jpg", "Page2Images/3YellowRubber3.jpg", "Page2Images/3YellowRubber4.jpg", "Page2Images/3YellowRubber5.jpg", "Page2Images/3YellowRubber6.jpg",
+                  "Page2Images/4GreenTree1.jpg", "Page2Images/4GreenTree2.jpg", "Page2Images/4GreenTree3.jpg", "Page2Images/4GreenTree4.jpg", "Page2Images/4GreenTree5.jpg", "Page2Images/4GreenTree6.jpg",
+                  "Page2Images/5BlueWaves1.jpg", "Page2Images/5BlueWaves2.jpg", "Page2Images/5BlueWaves3.jpg", "Page2Images/5BlueWaves4.jpg", "Page2Images/5BlueWaves5.jpg", "Page2Images/5BlueWaves6.jpg",
+                  "Page2Images/6PurpleFlowers1.jpg", "Page2Images/6PurpleFlowers2.jpg", "Page2Images/6PurpleFlowers3.jpg", "Page2Images/6PurpleFlowers4.jpg", "Page2Images/6PurpleFlowers5.jpg", "Page2Images/6PurpleFlowers6.jpg"]
+
     var image = document.getElementById("picture");
-    var randomNumber = Math.floor((Math.random()*2));
+    var randomNumber = Math.floor((Math.random()*imageArray.length));
     image.src = (imageArray[randomNumber]);
 }
